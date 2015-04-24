92fb84ecd83d3f02fbe9677230fe1eaa5c70b57c
xenhaoAC <xenhao.yap@alphacrossing.com>
2015-04-24 14:32:03 +0800

    test

--- app/src/main/java/com/example/xenhao/map2/MapsActivity.java
+++ app/src/main/java/com/example/xenhao/map2/MapsActivity.java
@@ -43,7 +43,8 @@ public class MapsActivity extends FragmentActivity {
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
      * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * call {@link #setUpMap()} once when {@link
+     * #mMap} is not null.
      * <p/>
      * If it isn't installed {@link SupportMapFragment} (and
      * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
