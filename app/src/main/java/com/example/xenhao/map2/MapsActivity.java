dce6ef39b43c7a935fc050031ff4b9a565841197
xenhaoAC <xenhao.yap@alphacrossing.com>
2015-06-19 10:56:01 +0800

    Slide-in drawer from the left added.

--- app/src/main/java/com/example/xenhao/map2/MapsActivity.java
+++ app/src/main/java/com/example/xenhao/map2/MapsActivity.java
@@ -1,10 +1,23 @@
 package com.example.xenhao.map2;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
@@ -13,6 +26,8 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity {
 
@@ -25,16 +40,68 @@ public class MapsActivity extends FragmentActivity {
     Toolbar toolbar;
     FloatingActionsMenu FAB;
 
+    //  Slide in drawer
+    private static String TAG = MapsActivity.class.getSimpleName();
+
+    ListView mDrawerList;
+    RelativeLayout mDrawerPane;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
+    ArrayList<NavItem> mNavItems = new ArrayList<NavItem>();
+    //***************************************************************//
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        // Slide in Drawer
+        mNavItems.add(new NavItem("Home", "Meetup Destination", R.drawable.ic_action_accept));
+        mNavItems.add(new NavItem("Preferences", "Change Preferences", R.drawable.ic_action_share));
+        mNavItems.add(new NavItem("About", "Get To Know Us", R.drawable.ic_action_about));
+
+        //  DrawerLayout
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        //  Populate the Navigation Drawer with options
+        mDrawerPane = (RelativeLayout) findViewById(R.id.drawerPane);
+        mDrawerList = (ListView) findViewById(R.id.navList);
+        DrawerListAdapter adapter = new DrawerListAdapter(this, mNavItems);
+        mDrawerList.setAdapter(adapter);
+
+        //  Drawer Item click listeners
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectItemFromDrawer(position);
+            }
+        });
 
 //        toolbar = (Toolbar)findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
         setUpMapIfNeeded();
         setUpFAB();
+    }
+
+    /*
+    * Called when a particular item from the navigation drawer
+    * is selected.
+    * */
+    private void selectItemFromDrawer(int position) {
+        Fragment fragment = new PreferencesFragment();
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.mainContent, fragment)
+                .commit();
+
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mNavItems.get(position).mTitle);
+
+        // Close the drawer
+        mDrawerLayout.closeDrawer(mDrawerPane);
     }
 
     private void setUpFAB() {
@@ -108,4 +175,64 @@ public class MapsActivity extends FragmentActivity {
         mMap.addMarker(new MarkerOptions().position(KUALA_LUMPUR).title("Kuala Lumpur").draggable(true));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(KUALA_LUMPUR, 10), 2000, null);
     }
+
+    class NavItem{
+        String mTitle;
+        String mSubtitle;
+        int mIcon;
+
+        public NavItem(String title, String subtitle, int icon){
+            mTitle = title;
+            mSubtitle = subtitle;
+            mIcon = icon;
+        }
+    }
+
+    class DrawerListAdapter extends BaseAdapter{
+        Context mContext;
+        ArrayList<NavItem> mNavItem;
+
+        public DrawerListAdapter(Context context, ArrayList<NavItem> navItems){
+            mContext = context;
+            mNavItem = navItems;
+        }
+
+        @Override
+        public int getCount(){
+            return mNavItem.size();
+        }
+
+        @Override
+        public Object getItem(int postion){
+            return mNavItem.get(postion);
+        }
+
+        @Override
+        public long getItemId(int position){
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            View view;
+
+            if(convertView == null){
+                LayoutInflater inflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.drawer_item, null);
+            }else{
+                view = convertView;
+            }
+
+            TextView titleView = (TextView)view.findViewById(R.id.title);
+            TextView subtitleView = (TextView)view.findViewById(R.id.subTitle);
+            ImageView iconView = (ImageView)view.findViewById(R.id.icon);
+
+            titleView.setText(mNavItem.get(position).mTitle);
+            subtitleView.setText(mNavItem.get(position).mSubtitle);
+            iconView.setImageResource(mNavItem.get(position).mIcon);
+
+            return view;
+        }
+    }
+
 }
